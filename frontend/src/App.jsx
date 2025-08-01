d3e67aa6276031f2daae3fe2850bea207be5dc49
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-01 17:03:25 -0400

    initial app setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>NASA Image Explorer</h1>
+      {/* routes and components here */}
+    </div>
+  );
+}
+
+export default App;
+
