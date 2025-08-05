df70431725d2ddbda555415ef83d82b1c4ae9192
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-04 21:16:51 -0400

    Update NavBar.jsx

--- frontend/src/components/Navbar/NavBar.jsx
+++ frontend/src/components/Navbar/NavBar.jsx
@@ -1,12 +1,18 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import './NavBar.css';
 
 export default function NavBar() {
   return (
     <nav className="navbar">
-      <Link to="/">Home</Link>
-      <Link to="/search">Search</Link>
-      <Link to="/favorites">Favorites</Link>
-      <Link to="/login">Login</Link>
+      <input type="checkbox" id="menu-toggle" className="menu-toggle" />
+      <label htmlFor="menu-toggle" className="burger">&#9776;</label>
+
+      <div className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/explore">Explore</Link>
+        <Link to="/favorites">Favorites</Link>
+        <Link to="/login">Login</Link>
+      </div>
     </nav>
-  )
+  );
 }
