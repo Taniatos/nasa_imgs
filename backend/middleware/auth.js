b6860da6b83bc6e6d16d67e4f1e51993888dcf70
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-07 18:25:49 -0400

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,3 +1,5 @@
+// Middleware to protect routes - it makes sure the user is authenticated
+
 export function ensureAuth(req, res, next) {
   if (req.isAuthenticated()) return next()
   return res.status(401).json({ message: 'Unauthorized' })
