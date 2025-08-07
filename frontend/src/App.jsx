8fb38d0d14c1efa7e71428dc173ba0b9db192aae
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-07 18:24:59 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,15 @@
-import { Routes, Route } from 'react-router-dom'
-import './styles/fonts.css';
-import './index.css';
+import { Routes, Route } from "react-router-dom";
+import "./styles/fonts.css";
+import "./index.css";
 
-import Home from './pages/Home/Home'
-import Explore from './pages/Explore/Explore'
-import Login from './pages/Login/Login'
-import Favorites from './pages/Favorites/Favorites'
+import Home from "./pages/Home/Home";
+import Explore from "./pages/Explore/Explore";
+import Login from "./pages/Login/Login";
+import Favorites from "./pages/Favorites/Favorites";
 
 function App() {
   return (
     <div className="app-container">
-  
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/explore" element={<Explore />} />
@@ -18,7 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
