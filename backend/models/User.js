9152bebc7cd00841b8e0ddcb80eb71c428639239
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-07 18:26:02 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,27 +1,27 @@
-import mongoose from 'mongoose'
-import bcrypt from 'bcrypt'
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-})
+  password: { type: String, required: true },
+});
 
-// Hash password *before* saving
-userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next()
+// Hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
   try {
-    const salt = await bcrypt.genSalt(10)
-    this.password = await bcrypt.hash(this.password, salt)
-    next()
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
   } catch (err) {
-    next(err)
+    next(err);
   }
-})
+});
 
 // Method to compare passwords
 userSchema.methods.comparePassword = function (candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password)
-}
+  return bcrypt.compare(candidatePassword, this.password);
+};
 
-const User = mongoose.model('User', userSchema)
-export default User
+const User = mongoose.model("User", userSchema);
+export default User;
