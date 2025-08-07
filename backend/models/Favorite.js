d1421e84461a26d559baf0b7786476195e404456
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-07 18:25:54 -0400

    Update Favorite.js

--- backend/models/Favorite.js
+++ backend/models/Favorite.js
@@ -1,11 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
+// Schema for a user's favorite image
 const favoriteSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  // One-to-many relationship: one User can have many Favorites
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  // The unique identifier for the image
   nasaId: { type: String, required: true },
   title: String,
   imageUrl: String,
-  dateSaved: { type: Date, default: Date.now }
-})
+  dateSaved: { type: Date, default: Date.now },
+});
 
-export default mongoose.model('Favorite', favoriteSchema)
+export default mongoose.model("Favorite", favoriteSchema);
