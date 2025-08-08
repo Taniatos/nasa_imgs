618409d84e7a22f60b2f3f9fa43b629d89efda33
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-08 16:28:32 -0400

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home/Home";
 import Explore from "./pages/Explore/Explore";
 import Login from "./pages/Login/Login";
 import Favorites from "./pages/Favorites/Favorites";
+import Admin from "./pages/Admin/Admin";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/explore" element={<Explore />} />
         <Route path="/favorites" element={<Favorites />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   );
