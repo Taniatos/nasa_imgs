662900ad801b610e97ffada3897900f8002b3ee3
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-08 16:25:14 -0400

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,6 +4,11 @@ import bcrypt from "bcrypt";
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
 });
 
 // Hash password before saving
