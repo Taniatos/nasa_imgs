162b97b061f4fb7833742e555f848a75348193e9
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-08 16:24:57 -0400

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,6 +1,14 @@
-// Middleware to protect routes - it makes sure the user is authenticated
-
+// Middleware to protect routes by making sure the user is authenticated
 export function ensureAuth(req, res, next) {
-  if (req.isAuthenticated()) return next()
-  return res.status(401).json({ message: 'Unauthorized' })
+  if (req.isAuthenticated()) return next();
+  return res.status(401).json({ message: "Unauthorized" });
+}
+
+// Middleware to ensure the authenticated user is an admin
+export function ensureAdmin(req, res, next) {
+  if (req.user && req.user.role === "admin") {
+    return next();
+  }
+  // If not an admin, send a 'Forbidden' status
+  return res.status(403).json({ message: "Forbidden: Admins only" });
 }
