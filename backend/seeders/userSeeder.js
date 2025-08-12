109fc536711c0e03bc38354ed6cbd1106498885e
Tetiana Korchynska <tanakorchynska@gmail.com>
2025-08-12 11:21:29 -0400

    Update userSeeder.js

--- backend/seeders/userSeeder.js
+++ backend/seeders/userSeeder.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import path from "path";
 import { fileURLToPath } from "url";
 import User from "../models/User.js";
+import Favorite from "../models/Favorite.js"; 
 
 // Configure the path to the .env file
 const __filename = fileURLToPath(import.meta.url);
@@ -11,20 +12,20 @@ dotenv.config({ path: path.resolve(__dirname, "../.env") });
 
 const seedUsers = async () => {
   try {
-    // Check if the MONGO_URI is loaded
     if (!process.env.MONGO_URI) {
       throw new Error(
         "MONGO_URI is not defined. Make sure your .env file is in the backend directory."
       );
     }
 
-    // Connect to the database
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB connected for seeding");
 
-    // Clear existing users to start fresh
+    // Clear existing users AND favorites
     await User.deleteMany({});
     console.log("Existing users cleared");
+    await Favorite.deleteMany({}); 
+    console.log("Existing favorites cleared");
 
     // Define the default users
     const usersData = [
@@ -40,7 +41,6 @@ const seedUsers = async () => {
       },
     ];
 
-    // Create user instances and save them individually to trigger the 'pre-save' hook for password hashing
     const userPromises = usersData.map((userData) => {
       const user = new User(userData);
       return user.save();
@@ -51,7 +51,6 @@ const seedUsers = async () => {
   } catch (error) {
     console.error("Error seeding database:", error);
   } finally {
-    // Disconnect from the database
     mongoose.disconnect();
     console.log("MongoDB disconnected");
   }
